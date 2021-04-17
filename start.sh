d8953e463f6c1f069349f4a9caa66c7b18d95e59
unhochoi <yms04089@kookmin.ac.kr>
2021-04-17 22:27:09 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ apt-get update
 apt-get install python3-pip -y
 pip3 install virtualenv
 cd /home/ubuntu
-git clone https://github.com/thejungwon/aws-sample-general-app
+git clone https://github.com/unhochoi/aws-sample-general-app
 cd aws-sample-general-app
+mkdir uploads
 virtualenv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
