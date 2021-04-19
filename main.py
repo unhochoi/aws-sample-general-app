dca0338afe1521cf93bc439d5e0116e1d4070070
unhochoi <yms04089@kookmin.ac.kr>
2021-04-19 18:36:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,35 +9,48 @@ import time
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions, MobileNetV2
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret key"
 
+inference_model = MobileNetV2()
+
 def getPrediction(filename):
-    # load the model
-    model = MobileNetV2()
-    # load an image from file
-    image = load_img('uploads/' + filename, target_size=(224, 224))
-    # convert the image pixels to a numpy array
+    image = load_img('static/uploads/' + filename, target_size=(224, 224))
     image = img_to_array(image)
-    # reshape data for the model
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    # prepare the images for the model
     image = preprocess_input(image)
-    # predict the probability across all output classes
-    yhat = model.predict(image)
-    # convert the probabilities to class labels
-    label = decode_predictions(yhat)
-    # retrieve the most likely result
-    
-    top1 = label[0][0]
-    return top1[1], top1[2]*100
+
+    all_result = inference_model.predict(image)
+    result = decode_predictions(all_result)[0]
+    result = [(img_class, label, str(round(acc * 100, 4)) + '%') for img_class, label, acc in result]
+    return result
+
+def get_instance_info():
+    try:
+        public_ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4", timeout=2).text
+        instance_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id", timeout=2).text
+        instance_type = requests.get("http://169.254.169.254/latest/meta-data/instance-type", timeout=2).text
+        region = requests.get("http://169.254.169.254/latest/meta-data/placement/region", timeout=2).text
+        avail_zone = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone", timeout=2).text
+
+        for info in [public_ip, instance_id, instance_type, avail_zone]:
+            flash(info)
+    except:
+        for i in range(4):
+            flash('Error')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
+    for i in range(10):
+        flash('')
+    get_instance_info()
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
@@ -50,14 +63,22 @@ def submit_file():
         if file.filename == '':
             flash('No file selected for uploading')
             return redirect(request.url)
-        if file:
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            label, acc = getPrediction(filename)
-            flash(label)
-            flash(acc)
-            flash(filename)
-            return redirect('/')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result = getPrediction(filename)
+            for top_result in result:
+                flash(top_result[1])
+                flash(top_result[2])
+            get_instance_info()
+            return render_template('index.html', filename=filename)
+        else:
+            flash('Allowed image types are -> png, jpg, jpeg, gif')
+            return redirect(request.url)
 
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80 ,debug=True)
