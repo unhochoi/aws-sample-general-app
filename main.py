c64b8589bfdf74b039a63810c19af47f30cd2b06
unhochoi <yms04089@kookmin.ac.kr>
2021-04-20 15:18:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 import os
 import requests
 import time
+import json
 
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions, MobileNetV2
@@ -31,16 +32,25 @@ def getPrediction(filename):
 
 def get_instance_info():
     try:
-        public_ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4", timeout=2).text
+        # instance_ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4", timeout=2).text
         instance_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id", timeout=2).text
         instance_type = requests.get("http://169.254.169.254/latest/meta-data/instance-type", timeout=2).text
-        region = requests.get("http://169.254.169.254/latest/meta-data/placement/region", timeout=2).text
         avail_zone = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone", timeout=2).text
 
-        for info in [public_ip, instance_id, instance_type, avail_zone]:
+        geo_info = requests.get('https://freegeoip.app/json')
+        geo_json = json.loads(geo_info.text)
+
+        geo_ip = geo_json['ip']
+        geo_country_name = geo_json['country_name']
+        geo_region_name = geo_json['region_name']
+        geo_time_zone = geo_json['time_zone']
+        geo_lat_lon = f"{geo_json['latitude']} / {geo_json['longitude']}"
+
+        for info in [geo_ip, instance_id, instance_type, avail_zone,
+        geo_country_name, geo_region_name, geo_time_zone, geo_lat_lon]:
             flash(info)
     except:
-        for i in range(4):
+        for i in range(8):
             flash('Error')
 
 def allowed_file(filename):
@@ -79,6 +89,31 @@ def submit_file():
 @app.route('/display/<filename>')
 def display_image(filename):
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
-        
+
+@app.route('/test', methods=['POST'])
+def curl_test():
+    return "SUCCESS"
+    # if request.method == 'POST':
+    #     if 'file' not in request.files:
+    #         return 'A'
+    #     file = request.files['file']
+    #     if file.filename == '':
+    #         return 'B'
+    #     if file and allowed_file(file.filename):
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #         result = getPrediction(filename)
+    #         for top_result in result:
+    #             print(top_result[1])
+    #             print(top_result[2])
+    #             # flash(top_result[1])
+    #             # flash(top_result[2])
+    #         # get_instance_info()
+    #         return 'SUCCESS'
+    #     else:
+    #         print('ERROR')
+    #         # flash('Allowed image types are -> png, jpg, jpeg, gif')
+    #         return 'C'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80 ,debug=True)
